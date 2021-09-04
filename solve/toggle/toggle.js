375fe4e6adfcae82717436e5d0ed99c2a45db87d
BlueGum24 <bluegumdev@gmail.com>
2021-09-04 12:48:41 +0200

    toggle solution

--- solve/toggle/toggle.js
+++ solve/toggle/toggle.js
@@ -3,6 +3,24 @@ export const ToggleModule = () => {
 
     //Write solution to the Toggle task inside this ToggleModule function.
     console.log('checking toggle...');
+
+    // select elements
+    const toggleText = document.querySelector(".toggle-text");
+    const toggleButton = document.querySelector(".toggle-button");
+
+    // toggle function
+    const toggle = () => {
+        if(toggleText.textContent === "OFF") {
+            toggleText.textContent = "ON";
+            toggleText.classList.add("on");
+        } else {
+            toggleText.textContent = "OFF";
+            toggleText.classList.remove("on");
+        }
+    }
+
+    // events
+    toggleButton.addEventListener("click", toggle);
     
 }
 
